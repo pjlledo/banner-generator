cabd8a91f9934d5254872b17ac0bae0affd5578a
Carles <carles2612@gmail.com>
2019-07-18 14:12:21 +0200

    Add media template

---
+++ src/components/templates/media/MediaCanvas.vue
@@ -0,0 +1,296 @@
+<template>
+  <div
+    :id="'bannerCanvas' + aspect"
+    :class="[
+      'banner-canvas',
+      'aspect-' + aspect,
+      aspect === '11' ? 'disposition-' + bannerProperties.disposition : '',
+      bannerProperties.localLabel ? 'has-local-label' : ''
+    ]"
+    v-if="bannerProperties">
+    <div class="blob blob-image">
+      <img :src="bannerProperties.picturePreview" alt="Imatge" v-if="bannerProperties.picturePreview" :style="objectPosition" />
+    </div>
+    <div class="blob blob-1"></div>
+    <div class="blob blob-2"></div>
+    <div class="media" contenteditable>
+      <div class="media-overtitle" contenteditable>
+        {{ bannerProperties.overtitle }}
+      </div>
+      <div class="media-title" contenteditable>
+        {{ bannerProperties.title }}
+      </div>
+      <div class="media-subtitle" contenteditable>
+        {{ bannerProperties.subtitle }}
+      </div>
+      <div class="media-details" contenteditable>
+        <b-icon icon="calendar-day"/>{{ bannerProperties.date }}
+      </div>
+      <div class="media-details" contenteditable>
+        <b-icon icon="clock"/>{{ bannerProperties.time }}
+      </div>
+      <div class="media-channel"></div>
+      <div class="media-programme"></div>
+    </div>
+    <div class="logo">
+      <img :src="logo" alt="Compromís" />
+      <div :class="{ 'logo-local-label': true, 'logo-local-label--long': bannerProperties.localLabel.length > 18 }" v-if="bannerProperties.localLabel">{{ bannerProperties.localLabel }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '@/assets/logo-compromis.svg'
+
+export default {
+  name: 'quote-canvas',
+
+  props: {
+    bannerProperties: Object,
+    aspect: String
+  },
+
+  data () {
+    return {
+      logo: Logo
+    }
+  },
+
+  computed: {
+    objectPosition: function () {
+      const objectPosition = (this.bannerProperties.pictureAspect === 'vertical')
+        ? '0% ' + this.bannerProperties.picturePos + '%'
+        : this.bannerProperties.picturePos + '% 0%'
+      return { objectPosition }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../../sass/variables";
+
+  .banner-canvas {
+    box-sizing: content-box;
+    position: relative;
+    width: 720px;
+    height: 720px;
+    border: 1px $white solid;
+    outline: 1px $gray-900 solid;
+    overflow: hidden;
+    transition: all .5s ease-in-out;
+    background: $white;
+  }
+
+  .event {
+    position: absolute;
+    top: 100px;
+    left: 0;
+    z-index: 40;
+    padding: 0 45px;
+    width: 260px;
+    z-index: 20;
+    transition: all .5s ease-in-out;
+    font-family: 'Compromis', serif;
+
+    &-title {
+      font-size: 40px;
+      line-height: 1.2;
+      color: $gray-900;
+      letter-spacing: -1px;
+      display: inline;
+      word-wrap: break-word;
+      font-family: 'Compromis', serif;
+      font-weight: bold;
+    }
+
+    &-details {
+      padding-top: .9rem;
+      display: flex;
+      align-items: center;
+      font-size: 19px;
+      letter-spacing: -0.5px;
+      width: 290px;
+      color: $gray-700;
+
+      .icon {
+        padding-right: .3rem;
+        svg {
+          width: 500px;
+          height: 500px;
+        }
+      }
+    }
+  }
+
+  .blob {
+    position: absolute;
+    width: 720px;
+    height: 720px;
+    overflow: hidden;
+    border-radius: $border-radius;
+    background: linear-gradient(45deg,$gradient-start,$gradient-end);
+    transform: rotate($rotation);
+    transition: all .5s ease-in-out;
+
+    &-1 {
+      top: -87%;
+      left: -74%;
+    }
+
+    &-2 {
+      left: -63%;
+      bottom: -74%;
+      z-index: 10;
+    }
+
+    &-image {
+      top: -15%;
+      right: -32%;
+      height: 680px;
+      z-index: 20;
+      background: $gray-300;
+      width: 620px;
+
+      img {
+        transform: rotate(-$rotation) scale(1.15);
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+
+  .logo {
+    display: flex;
+    position: absolute;
+    right: 35px;
+    bottom: 15px;
+    align-items: center;
+
+    img {
+      height: 30px;
+    }
+
+    &-local-label {
+      border-left: 1px $gray-400 solid;
+      margin-left: .75rem;
+      padding-left: .75rem;
+      font-size: 1.25rem;
+      letter-spacing: -.3px;
+      line-height: 1;
+      white-space: nowrap;
+
+      &--long {
+        font-size: .95rem;
+        white-space: normal;
+        width: 90px;
+      }
+    }
+  }
+
+  .hashtag {
+    position: absolute;
+    z-index: 30;
+    bottom: 20px;
+    left: 35px;
+    color: $white;
+    font-weight: bold;
+    font-size: 20px;
+    letter-spacing: -.3px;
+  }
+
+  .has-local-label {
+    .blob-2 {
+      left: -60%;
+    }
+  }
+
+  // Story aspect
+  .aspect-916 {
+    width: 405px;
+
+    .blob {
+      &-1 {
+        display: none;
+      }
+
+      &-2 {
+        left: -110%;
+        bottom: -94%;
+      }
+
+      &-image {
+        height: 540px;
+        top: -160px;
+        left: -12px;
+        border-bottom-right-radius: 0;
+      }
+    }
+
+    .event {
+      top: 360px;
+
+      &-title {
+        font-size: 34px;
+        line-height: 1.42;
+        color: white;
+        padding: 0 10px;
+        border-radius: 2px;
+        background: linear-gradient(45deg,$gradient-start,$gradient-end);
+        letter-spacing: -1px;
+        display: inline;
+        box-decoration-break: clone;
+        -webkit-box-decoration-break: clone;
+      }
+    }
+
+    .logo {
+      display: none;
+    }
+  }
+
+  // Quote on top
+  .disposition-1 {
+    .event {
+      top: 38px;
+
+      &-details-wrapper {
+        position: absolute;
+        bottom: -400px;
+        right: -451px;
+      }
+    }
+
+    &.has-local-label {
+      .blob-2 {
+        left: -60%;
+        top: -88%;
+      }
+    }
+
+    .hashtag {
+      bottom: 668px;
+    }
+
+    .blob {
+      &-1 {
+        left: 69%;
+        top: -60%;
+      }
+
+      &-2 {
+        display: none;
+      }
+
+      &-image {
+        left: auto;
+        top: auto;
+        bottom: -108px;
+        right: 281px;
+        height: 593px;
+        width: 650px;
+      }
+    }
+  }
+</style>
---
+++ src/components/templates/media/MediaPane.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <transition name="slide">
+      <b-tabs type="is-toggle" size="is-small" v-model="properties.disposition" v-if="!aspect" class="banner-disposition" expanded>
+        <b-tab-item label="Frase esquerra"></b-tab-item>
+        <b-tab-item label="Frase dalt"></b-tab-item>
+      </b-tabs>
+    </transition>
+    <b-field label="Titol">
+      <b-input placeholder="Acte Central a València" v-model="properties.title" maxlength="60"></b-input>
+    </b-field>
+    <b-field label="Data">
+      <b-input placeholder="21 de Febrer" v-model="properties.date"></b-input>
+    </b-field>
+    <b-field label="Hora">
+      <b-input placeholder="19h" v-model="properties.time"></b-input>
+    </b-field>
+    <b-field label="Foto" class="image-upload-field">
+      <b-upload @input="updateImage" drag-drop>
+        <section class="section">
+          <div class="content has-text-centered" v-if="!properties.picture">
+            <b-icon icon="upload" size="is-large" />
+            <p>Arrosega la foto</p>
+          </div>
+          <div class="content has-text-centered" v-else>
+            <p>{{ properties.picture.name }}</p>
+          </div>
+        </section>
+      </b-upload>
+      <b-button v-if="properties.picture" @click="properties.picture = null; properties.picturePreview = null" class="remove-image" type="is-danger">
+        <b-icon icon="times" />
+      </b-button>
+    </b-field>
+    <b-field label="Posició de la imatge">
+      <range-slider name="points" :min="0" :max="100" v-model="properties.picturePos" />
+    </b-field>
+    <transition name="slide">
+      <div v-if="!aspect">
+        <b-switch v-model="properties.hasLocalLabel">
+          Afegir text al logo
+        </b-switch>
+        <transition name="slide">
+          <div v-if="properties.hasLocalLabel" class="local-label">
+            <b-field>
+              <b-input placeholder="Alacant" v-model="properties.localLabel" maxlength="48"></b-input>
+            </b-field>
+          </div>
+        </transition>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { EventBus } from '@/event-bus.js'
+import RangeSlider from '@/utils/RangeSlider.vue'
+
+export default {
+  name: 'quote-pane',
+
+  components: {
+    RangeSlider
+  },
+
+  data () {
+    return {
+      properties: {
+        disposition: 0,
+        picture: null,
+        picturePreview: '',
+        picturePos: 50,
+        title: '',
+        date: '',
+        time: '',
+        place: '',
+        hasLocalLabel: false,
+        localLabel: ''
+      },
+      aspect: 0
+    }
+  },
+
+  // Emit state to parent component
+  watch: {
+    properties: {
+      handler: function (properties) {
+        this.$emit('updated', properties)
+      },
+      deep: true
+    }
+  },
+
+  created () {
+    // Emit properties to canvas on component load
+    this.$emit('updated', this.properties)
+
+    // Update aspect
+    EventBus.$on('aspectUpdated', (aspect) => { this.aspect = aspect })
+  },
+
+  methods: {
+    updateImage (image) {
+      this.properties.picture = image
+      this.properties.picturePreview = URL.createObjectURL(image)
+
+      const img = new Image()
+      img.onload = () => {
+        this.properties.pictureAspect = (img.width / img.height > 1) ? 'horizontal' : 'vertical'
+      }
+      img.src = this.properties.picturePreview
+    },
+
+    updateHashtag (hashtag) {
+      if (!hashtag) {
+        this.properties.hashtag = ''
+        return
+      }
+
+      if (hashtag[0] === '#') {
+        this.properties.hashtag = hashtag
+      } else {
+        this.properties.hashtag = '#' + hashtag
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .field {
+    position: relative;
+  }
+
+  .section {
+    padding: 2rem 1.5rem;
+  }
+
+  .remove-image {
+    position: absolute;
+    right: 0;
+    top: 2rem;
+  }
+
+  .hashtag {
+    margin-top: .25rem;
+  }
+
+  .local-label {
+    margin-top: .75rem;
+  }
+
+  .image-upload-field {
+
+    &.has-addons {
+      flex-direction: column;
+      width: 100%;
+    }
+  }
+</style>
+
+<style lang="scss">
+  .textarea-small .textarea {
+    min-height: 4.3rem;
+  }
+</style>
