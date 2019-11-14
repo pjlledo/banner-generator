b12f9d6fdfb7008224f90beaa1847214819ab6a6
Guillem <guillem@disedit.com>
2019-11-14 10:52:12 +0100

    Add party logos

--- src/components/templates/comparison/presets.js
+++ src/components/templates/comparison/presets.js
@@ -1,30 +1,33 @@
-import LaVanguardiaSVG from './logos/lavanguardia.svg'
+import PpSVG from './logos/pp.svg'
+import CsSVG from './logos/cs.svg'
+import VoxSVG from './logos/vox.svg'
+import PsoeSVG from './logos/psoe.svg'
 
 const sources = [
   {
     id: 'pp',
     name: 'Partido Popular',
-    logo: LaVanguardiaSVG,
-    color: '#235690'
+    logo: PpSVG,
+    color: '#0056a3'
   },
   {
     id: 'cs',
     name: 'Ciudadanos',
-    logo: LaVanguardiaSVG,
-    color: '#071C49'
+    logo: CsSVG,
+    color: '#ff4f00'
   },
   {
     id: 'vox',
     name: 'VOX',
-    logo: LaVanguardiaSVG,
-    color: '#ED7B38'
+    logo: VoxSVG,
+    color: '#5ac035'
   },
   {
     id: 'psoe',
     name: 'PSOE',
-    logo: LaVanguardiaSVG,
-    color: '#ED7B38'
-  },
+    logo: PsoeSVG,
+    color: '#e30613'
+  }
 ]
 
 export default sources
