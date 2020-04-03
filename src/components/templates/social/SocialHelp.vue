71fcf98aabecbb80bc6cde0bb080d1adcc7a8b43
pjlledo <pls@usebox.net>
2020-04-03 10:09:42 +0200

    Update Help message in Social Canvas

--- src/components/templates/social/SocialHelp.vue
+++ src/components/templates/social/SocialHelp.vue
@@ -5,18 +5,14 @@
     </p>
     <p>
       Recomanem usar una <strong>foto representativa del poble o comarca</strong>, que puga ser reconeguda amb facilitat al costat de logo, per exemple un edifici
-      representatiu (com el Micalet en el cas de València) o un paisatge fàcil de reconéixer.
+      representatiu o un paisatge fàcil de reconéixer.
     </p>
     <p>
-      Us recomanem no utilitzar altres ferramentes per a canviar la disposició de la careta respecte a la foto del poble. L'element principal és la careta,
-      i ha de poder-se identificar amb facilitat en formats menuts.
-    </p>
-    <p>
-      Els dies importants (Orgull, 8 de Març), podeu utilitzar aquesta ferramenta per a modificar la foto de perfil.
+      Els dies importants (Orgull, 8 de Març, 9 d'octubre), podeu utilitzar aquesta ferramenta per a modificar la foto de perfil.
     </p>
 
     <div class="help-examples">
-      <img style="border-radius: 100%" src="./examples/11.png" alt="Exemple 1:1" />
+      <img style="border-radius: 100%" src="./examples/JovesPValc.jpg" alt="Exemple 1:1" />
     </div>
   </div>
 </template>
