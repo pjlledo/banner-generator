c09461fa12c4c1d54b2ce6dc556b531f33f6fc8b
pjlledo <pls@usebox.net>
2020-04-07 13:25:22 +0200

    Canvis en el selector de icones socials

--- src/components/templates/social/SocialCanvas.vue
+++ src/components/templates/social/SocialCanvas.vue
@@ -11,17 +11,15 @@
 <script>
 import CanvasMixin from '@/mixins/canvas-mixin'
 import Careta from '@/utils/Careta'
-
 export default {
   name: 'social-canvas',
-
   mixins: [CanvasMixin],
-
   components: {
     Careta
   }
 }
 </script>
+
 
 <style lang="scss" scoped>
   @import "../../../sass/variables";
@@ -30,20 +28,6 @@ export default {
     width: 100%;
     height: 100%;
     object-fit: cover;
-     background-image: linear-gradient(180deg,
-      rgba(239,64,77,1) 0%,
-      rgba(239,64,77,1) 12.5%,
-      rgba(247,146,38,1) 12.5%,
-      rgba(247,146,38,1) 25%,
-      rgba(255,209,15,1) 25%,
-      rgba(255,209,15,1) 37.5%,
-      rgba(127,195,71,1) 37.5%,
-      rgba(127,195,71,1) 50%,
-      rgba(85,164,219,1) 50%,
-      rgba(85,164,219,1) 62,5%,
-      rgba(161,74,156,1) 62,5%,
-      rgba(161,74,156,1) 75%);
-    background-blend-mode: multiply;
   }
 
   .careta {
@@ -53,10 +37,6 @@ export default {
     width: 500px;
     left: 100px;
     top: 100px;
-  }
-
-  .banner-canvas {
-   
-
+    filter: drop-shadow(0px 0px 10px rgba(0,0,0,.5));
   }
 </style>
--- src/utils/CaretaSelector.vue
+++ src/utils/CaretaSelector.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <ul :class="{'careta-selector': true, 'is-rounded': isRounded }">
-      <li :class="{'normal' : true, 'active': value === 'normal'}" @click="$emit('input', 'normal')" 
+      <li :class="{'normal' : true, 'active': value === 'mono'}" @click="$emit('input', 'mono')" 
       style=" background: #f4813e;">
           <careta v-if="!isRounded" logo-style="mono"></careta>
       </li>
       <li :class="{'feminista' : true, 'active': value === 'feminista'}" @click="$emit('input', 'feminista')"
-      style=" background: #7D3A81">
-          <careta v-if="!isRounded" logo-style="mono"></careta>
+      style=" background: #662D91">
+          <careta v-if="!isRounded" logo-style="feminista"></careta>
       </li>
       <li :class="{'lgtb' : true, 'active': value === 'lgtb'}" @click="$emit('input', 'lgtb')">
-          <careta v-if="!isRounded" logo-style="mono"></careta>
+          <careta v-if="!isRounded" logo-style="lgtb"></careta>
       </li>
       <li :class="{'quatri' : true, 'active': value === 'quatri'}" @click="$emit('input', 'mono')">
-	      <careta v-if="!isRounded" logo-style="mono"></careta>
+	      <careta v-if="!isRounded" logo-style="quatri"></careta>
 	    </li>
     </ul>
     <b-message v-if="value === 'feminista' && isRounded === false" type="is-warning" size="is-small">
@@ -65,7 +65,7 @@ export default {
   li {
     border: .25rem solid $gray-200;
     border-radius: .5rem;
-	background: $gray-200;
+  	background: $gray-200;
     transition: .25s ease-in-out;
 
     svg {
@@ -88,41 +88,41 @@ export default {
       }
     }
     &.lgtb {
-      background-image: linear-gradient(180deg,
-        rgba(239,64,77,1) 0%,
-        rgba(239,64,77,1) 16.6%,
-        rgba(247,146,38,1) 16.6%,
-        rgba(247,146,38,1) 33.2%,
-        rgba(255,209,15,1) 33.2%,
-        rgba(255,209,15,1) 49.8%,
-        rgba(127,195,71,1) 49.8%,
-        rgba(127,195,71,1) 66.4%,
-        rgba(85,164,219,1) 66.4%,
-        rgba(85,164,219,1) 83%,
-        rgba(161,74,156,1) 83%,
-        rgba(161,74,156,1) 100%);
+      background-image: linear-gradient(90deg,
+        rgba(234,32,36,1) 0%,
+        rgba(234,32,36,1) 16.6%,
+        rgba(246,150,30,1) 16.6%,
+        rgba(246,150,30,1) 33.2%,
+        rgba(244,232,22,1) 33.2%,
+        rgba(244,232,22,1) 49.8%,
+        rgba(18,153,71,1) 49.8%,
+        rgba(18,153,71,1) 66.4%,
+        rgba(58,84,162,1) 66.4%,
+        rgba(58,84,162,1) 83%,
+        rgba(196,39,142,1) 83%,
+        rgba(196,39,142,1) 100%);
     }
 
     &.quatri {
-      background-image: linear-gradient(180deg,
-        rgba(255,255,0,1) 0%,
-        rgba(255,255,0,1) 11.1%,
-        rgba(255,0,0,1) 11.1%,
-        rgba(255,0,0,1) 22.2%,
-        rgba(255,255,0,1) 22.2%,
-        rgba(255,255,0,1) 33.3%,
-        rgba(255,0,0,1) 33.3%,
-        rgba(255,0,0,1) 44.4%,
-        rgba(255,255,0,1) 44.4%,
-        rgba(255,255,0,1) 55.5%,
-        rgba(255,0,0,1) 55.5%,
-        rgba(255,0,0,1) 66.6%,
-        rgba(255,255,0,1) 66.6%,
-        rgba(255,255,0,1) 77.7%,
-        rgba(255,0,0,1) 77.7%,
-        rgba(255,0,0,1) 88.8%,
-        rgba(255,255,0,1) 88.8%,
-        rgba(255,255,0,1) 100%);
+      background-image: linear-gradient(90deg,
+        rgba(252,221,9,1) 0%,
+        rgba(252,221,9,1) 11.1%,
+        rgba(218,18,26,1) 11.1%,
+        rgba(218,18,26,1) 22.2%,
+        rgba(252,221,9,1) 22.2%,
+        rgba(252,221,9,1) 33.3%,
+        rgba(218,18,26,1) 33.3%,
+        rgba(218,18,26,1) 44.4%,
+        rgba(252,221,9,1) 44.4%,
+        rgba(252,221,9,1) 55.5%,
+        rgba(218,18,26,1) 55.5%,
+        rgba(218,18,26,1) 66.6%,
+        rgba(252,221,9,1) 66.6%,
+        rgba(252,221,9,1) 77.7%,
+        rgba(218,18,26,1) 77.7%,
+        rgba(218,18,26,1) 88.8%,
+        rgba(252,221,9,1) 88.8%,
+        rgba(252,221,9,1) 100%);
     }
   }
 
@@ -141,22 +141,18 @@ export default {
       border-color: $white;
       border-width: .2rem;
       box-shadow: 0 0 0 2px $gray-400;
-
       &:hover {
         box-shadow: 0 0 0 2px $gray-600;
       }
-
       &.active {
         box-shadow: 0 0 0 2px $orange;
       }
-
       &.feminista {
         background: $gradient-feminista;
       }
       &.lgtb {
         background: $gradient-lgtb;
-      }
-      
+      }      
       &.quatri {
       background: $gradient-quatri;
       }
