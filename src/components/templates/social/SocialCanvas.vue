ef6edb53a9abcb2fcef9dce8dae586ecba3b7f46
pjlledo <pls@usebox.net>
2020-04-15 16:53:36 +0200

    Adaptació Joves PV icona XXSS

--- src/components/templates/social/SocialCanvas.vue
+++ src/components/templates/social/SocialCanvas.vue
@@ -1,11 +1,14 @@
 <template>
   <div
     :id="'bannerCanvas' + aspect"
-    class='banner-canvas'
+    class='banner-canvas' 
     v-if="banner">
-     <img class="picture" :src="banner.picturePreview" alt="Imatge" v-if="banner.picturePreview" :style="objectPosition" />
-     <careta class="careta" :logo-style="banner.logoStyle"></careta>
+      <div :id="'bannerCanvas' + aspect + banner.logoStyle">
+        <img class="picture" :src="banner.picturePreview" alt="Imatge" v-if="banner.picturePreview" :style="objectPosition" />
+        <careta class="careta" :logo-style="banner.logoStyle"></careta>
+      </div>
   </div>
+
 </template>
 
 <script>
@@ -27,15 +30,65 @@ export default {
     width: 100%;
     height: 100%;
     object-fit: cover;
+    mix-blend-mode: luminosity;
   }
 
   .careta {
     position: absolute;
-    transform: rotate($rotation);
     z-index: 10;
     width: 500px;
     left: 100px;
     top: 100px;
     filter: drop-shadow(0px 0px 10px rgba(0,0,0,.5));
   }
+
+  #bannerCanvas11mono {
+    background-color: #e85d0f;
+    height: 720px;
+  }
+
+  #bannerCanvas11feminista {
+    background-color: #662D91;
+    height: 720px;
+  }
+
+  #bannerCanvas11lgtb {
+    background-image: linear-gradient(90deg,
+    rgba(234,32,36,1) 0%,
+    rgba(234,32,36,1) 16.6%,
+    rgba(246,150,30,1) 16.6%,
+    rgba(246,150,30,1) 33.2%,
+    rgba(244,232,22,1) 33.2%,
+    rgba(244,232,22,1) 49.8%,
+    rgba(18,153,71,1) 49.8%,
+    rgba(18,153,71,1) 66.4%,
+    rgba(58,84,162,1) 66.4%,
+    rgba(58,84,162,1) 83%,
+    rgba(196,39,142,1) 83%,
+    rgba(196,39,142,1) 100%);
+    height: 720px;
+ } 
+
+#bannerCanvas11quatri {
+  background-image: linear-gradient(90deg,
+  rgba(252,221,9,1) 0%,
+  rgba(252,221,9,1) 11.1%,
+  rgba(218,18,26,1) 11.1%,
+  rgba(218,18,26,1) 22.2%,
+  rgba(252,221,9,1) 22.2%,
+  rgba(252,221,9,1) 33.3%,
+  rgba(218,18,26,1) 33.3%,
+  rgba(218,18,26,1) 44.4%,
+  rgba(252,221,9,1) 44.4%,
+  rgba(252,221,9,1) 55.5%,
+  rgba(218,18,26,1) 55.5%,
+  rgba(218,18,26,1) 66.6%,
+  rgba(252,221,9,1) 66.6%,
+  rgba(252,221,9,1) 77.7%,
+  rgba(218,18,26,1) 77.7%,
+  rgba(218,18,26,1) 88.8%,
+  rgba(252,221,9,1) 88.8%,
+  rgba(252,221,9,1) 100%);
+  height: 720px;
+}
 </style>
--- src/components/templates/social/SocialPane.vue
+++ src/components/templates/social/SocialPane.vue
@@ -36,7 +36,7 @@ export default {
   data () {
     return {
       properties: {
-        logoStyle: 'normal'
+        logoStyle: 'mono'
       }
     }
   },
